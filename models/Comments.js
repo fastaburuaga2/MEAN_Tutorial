0ba301af9251e300c3a94358960c11e96a4e804a
fastaburuaga22 <fastaburuaga@uc.cl>
2015-10-12 17:09:23 +0800

    first commit

---
+++ models/Comments.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+
+var CommentSchema = new mongoose.Schema({
+  body: String,
+  author: String,
+  upvotes: {type: Number, default: 0},
+  post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post' }
+});
+
+CommentSchema.methods.upvote = function(cb) {
+  this.upvotes += 1;
+  this.save(cb);
+  };
+
+CommentSchema.methods.delete = function(cb) {
+  this.remove;
+  this.save(cb);
+  };
+
+mongoose.model('Comment', CommentSchema);
